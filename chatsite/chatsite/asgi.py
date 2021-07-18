db87fbb2071c6e81c88641b14d5bc7f53e130d3e
yantsenter <yantsentser@gmail.com>
2021-07-18 17:47:52 +0200

    push get_asgi_application after settings env set

--- chatsite/chatsite/asgi.py
+++ chatsite/chatsite/asgi.py
@@ -13,10 +13,9 @@ import django
 
 from django.core.asgi import get_asgi_application
 
-django_asgi_app = get_asgi_application()
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatsite.chatsite.settings')
 django.setup()
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
