3646257e08e5fc374451a358c044baf244c8aa4a
yantsenter <yantsentser@gmail.com>
2021-07-18 17:55:27 +0200

    remove django.setup()

--- chatsite/chatsite/asgi.py
+++ chatsite/chatsite/asgi.py
@@ -14,7 +14,6 @@ import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatsite.chatsite.settings')
-django.setup()
 django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
