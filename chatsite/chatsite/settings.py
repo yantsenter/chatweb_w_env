b26d9014ad625b1c39efe9919f7063ba3c4bef8a
yantsenter <yantsentser@gmail.com>
2021-07-19 15:34:01 +0200

    revert settings

--- chatsite/chatsite/settings.py
+++ chatsite/chatsite/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'channels',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chatsite.chat',
 ]
 
 MIDDLEWARE = [
